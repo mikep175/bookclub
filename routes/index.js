518cac291b608fa59bb84fc84b61bb831ef35271
mikep175 <mikeycp1@gmail.com>
2015-04-14 01:55:59 -0400

    added post for homepage

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.post('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
